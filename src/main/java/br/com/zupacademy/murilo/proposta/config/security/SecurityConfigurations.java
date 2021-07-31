0a7114c3b21d2cf4f1ca27ef0c60405131bcbba7
Murilo Badoco <guerreiro_murilo@hotmail.com>
2021-07-31 19:57:00 -0300

    create: Autenticação com o Keycloak

---
+++ src/main/java/br/com/zupacademy/murilo/proposta/config/security/SecurityConfigurations.java
@@ -0,0 +1,33 @@
+package br.com.zupacademy.murilo.proposta.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.authorizeRequests(authorize ->
+        		authorize
+        			//.antMatchers(HttpMethod.POST,"/propostas").hasAuthority("SCOPE_propostas")
+        			//.antMatchers(HttpMethod.GET,"/propostas/**").hasAuthority("SCOPE_propostas")
+        			//.antMatchers("/biometrias/**").hasAuthority("SCOPE_biometrias")
+        			//.antMatchers(HttpMethod.GET,"/actuator/**").hasAuthority("SCOPE_actuator")
+        			.anyRequest().authenticated()
+        	)
+        	.cors()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .csrf().disable()
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
